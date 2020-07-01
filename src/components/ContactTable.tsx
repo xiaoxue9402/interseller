8cbf43e0902c3cf8b5d2853dba2a8bf77a2f5656
xiaoxue9402 <shirleyyz9402@gmail.com>
2020-07-01 19:06:08 -0400

    Initial commit and layout

---
+++ src/components/ContactTable.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import MaterialTable, { Column } from 'material-table';
+
+interface Row {
+  firstName: string;
+  lastName: string;
+  label: string;
+  organization: string;
+  email: string;
+  phone: string;
+}
+
+interface TableState {
+  columns: Array<Column<Row>>;
+  data: Row[];
+}
+
+const contactsTable = () => {
+  const [state, setState] = React.useState<TableState>({
+    columns: [
+      { title: 'First Name', field: 'firstName' },
+      { title: 'Last Name', field: 'lastName' },
+      { title: 'Label', field: 'label' },
+      { title: 'Organization', field: 'organization' },
+      { title: 'Email', field: 'email' },
+      { title: 'Phone', field: 'phone' },
+    ],
+    data: [
+      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+      {
+        name: 'Zerya Betül',
+        surname: 'Baran',
+        birthYear: 2017,
+        birthCity: 34,
+      },
+    ],
+  });
+
+
+}
+
+export default contactsTable;
---
+++ src/components/MainArea.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class MainArea extends React.Component {
+  
+}
