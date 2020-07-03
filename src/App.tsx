99efcdbfcc40d241205b661c5b0ac2354d236679
xiaoxue9402 <shirleyyz9402@gmail.com>
2020-07-03 14:02:22 -0400

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,37 @@ import React from "react";
 
 import "./App.css";
 import MainArea from "./components/MainArea";
-import {Data} from "./components/interface"
+import { Data } from "./components/interface";
 
-  function App(): JSX.Element {
-  const [contacts, setContacts] = React.useState<Array<Data>>([])
-  async () => {
-    const getContacts = async () => {
-      return await fetch(
-        " https://interseller.pipedrive.com/api/v1/persons?api_token=2ce39665b10b200e6da1f49bfe6014429a98a3ea"
-      )
-        .then((res) => res.json())
-        .then((res) => res.data);
-    };
-    setContacts([await getContacts()])
-  }
-  
-  return (
-    <div className="App">
-      <MainArea 
-        contacts={contacts}
-      />
-    </div>
-  )
+interface State {
+  contacts: any[];
 }
 
-export default App;
+export default class App extends React.Component {
+   state: State = {
+    contacts: [],
+  }
+  componentDidMount() {
+    fetch(
+      " https://interseller.pipedrive.com/api/v1/persons?api_token=2ce39665b10b200e6da1f49bfe6014429a98a3ea"
+    )
+      .then((res) => res.json())
+      .then((res) => this.setState({contacts: [...res.data]}));
+  }
+  render() {
+    const contactInfo: Data[] = this.state.contacts.map((contact) => {
+      return {
+        firstName: contact.first_name,
+        lastName: contact.last_name,
+        organization: contact.org_name,
+        email: contact.email[0].value,
+        phone: contact.phone[0].value,
+      };
+    })
+    return (
+      <>
+        <MainArea contacts={this.state.contacts} />
+      </>
+    )
+  }
+}
